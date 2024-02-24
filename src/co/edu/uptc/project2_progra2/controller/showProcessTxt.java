62ab609603df8e1e92c611280fab5b9fe8ebe326
Manuel.cruz <Mcruzchaparro01@gmail.com>
2024-02-24 15:35:24 -0500

    extraer en txt e img archivo txt en base 64

--- src/co/edu/uptc/project2_progra2/controller/showProcessTxt.java
+++ src/co/edu/uptc/project2_progra2/controller/showProcessTxt.java
@@ -1,14 +1,15 @@
 package co.edu.uptc.project2_progra2.controller;
-import java.io.IOException;
 
 import co.edu.uptc.project2_progra2.services.processTxt;
+
 public class showProcessTxt {
     public void showTxt () {
         processTxt processTxt = new processTxt();
         try {
-            System.out.println(processTxt.readFile());
-        } catch (IOException e) {
-            e.printStackTrace();
+            processTxt.setPathName("\\C:\\Users\\FAMILIA CRUZ\\OneDrive\\Documentos\\progra2\\imgbase64.jpg");
+            processTxt.deCodeBase64Img();
+        } catch (Exception e) {
+            System.out.println("Hubo un error");
         }
     }
 }
--- src/co/edu/uptc/project2_progra2/services/processTxt.java
+++ src/co/edu/uptc/project2_progra2/services/processTxt.java
@@ -1,25 +1,43 @@
 package co.edu.uptc.project2_progra2.services;
+
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-public class processTxt {
-    public String path = "C:\\Users\\FAMILIA CRUZ\\OneDrive\\Documentos\\progra2\\archivobase64.txt";
+import java.util.Base64;
 
-    public boolean existFile () {
-        File file = new File(path);
-        return file.exists();
+import javax.imageio.ImageIO;
+
+public class processTxt {
+    private String path = "";
+    public void setPathName (String path) {
+        this.path = path;
+    }
+    public String readFile() throws IOException {
+        File doc = new File(path);
+        BufferedReader obj = new BufferedReader(new FileReader(doc));
+        String txt = "";
+        String strng;
+        while ((strng = obj.readLine()) != null) {
+            txt += strng;
+        }
+        obj.close();
+        return txt;
     }
 
-    public String readFile () throws IOException {
-        String texto = "";
-        File file = new File(path);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-        while ((texto = br.readLine()) != null) {
-            System.out.println(texto);
-        }
-        fr.close();
-        return texto;
+    public String deCodeBase64Txt() throws Exception {
+        String base64 = readFile();
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+        String decodedString = new String(decodedBytes);
+        return decodedString;
+    }
+
+    public void deCodeBase64Img () throws IOException {
+        File outputFile = new File("C:\\Users\\FAMILIA CRUZ\\OneDrive\\Documentos\\progra2\\image.jpg");
+        String base64 = readFile();
+        byte[] bytesDecode = Base64.getDecoder().decode(base64);
+        BufferedImage image = javax.imageio.ImageIO.read(new java.io.ByteArrayInputStream(bytesDecode));
+        ImageIO.write(image, "jpg", outputFile);
     }
 }
